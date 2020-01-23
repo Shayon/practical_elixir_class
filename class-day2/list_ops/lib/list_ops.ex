96d1246c1dc5b14b268f8c7048034957170e4575
Tyler Naumu <tyler@procore.com>
2020-01-23 09:21:24 -0800

    WIP

--- class-day2/list_ops/lib/list_ops.ex
+++ class-day2/list_ops/lib/list_ops.ex
@@ -3,7 +3,7 @@ defmodule ListOps do
   Implements list operations from scratch.  Will not use any external modules OR Kernel.++/2.
   """
 
-  def reduce(_, _, _) do
+  def reduce(list, acc, reducer) do
     []
   end
 end
