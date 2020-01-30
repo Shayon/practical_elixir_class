935e42f2cd9880c0a0b10910ba0a1a5737ce318f
Glenn Meyer <glenn.meyer@procore.com>
2020-01-30 12:00:44 -0600

    out of time

--- class-day3/learn_about_processes/lib/my_gen_server.ex
+++ class-day3/learn_about_processes/lib/my_gen_server.ex
@@ -5,7 +5,11 @@ defmodule MyGenServer do
     end
 
     pid = spawn_link(__MODULE__, :init, [module, atom_list])
-    Process.register(pid, keyword_list[:name])
+
+    if(keyword_list[:name]) do
+        Process.register(pid, keyword_list[:name])
+    end
+
     {:ok, pid}
   end
 
@@ -22,13 +26,19 @@ defmodule MyGenServer do
           {:reply, reply, new_state} = module.handle_call(msg, pid, state)
           send(pid, {:response, reply})
           new_state
+        {:cast, msg} ->
+          {:no_reply, new_state} = module.handle_cast(msg, state)
+          new_state
       end
 
     listener(module, state)
   end
 
   def call(server, msg) do
-    pid = Process.whereis(server)
+    pid = case server do
+        server when is_pid(server) -> server
+        server -> Process.whereis(server)
+    end
     send(pid, {:call, msg, self()})
 
     receive do
@@ -39,5 +49,14 @@ defmodule MyGenServer do
   def start(module, args) do
     pid = spawn(__MODULE__, :init, [module, args])
     {:ok, pid}
+  end
+
+  def cast(server, msg) do
+    pid = case server do
+        server when is_pid(server) -> server
+        server -> Process.whereis(server)
+    end
+    send(pid, {:cast, msg})
+    :ok
   end
 end
