4af66b02f34dbd2d0319412ad6c4d80b52920488
Michael Cola <michaelc@procore.com>
2020-01-30 11:10:29 -0600

    Start writing raise condition

--- class-day2/list_ops/test/list_ops_test.exs
+++ class-day2/list_ops/test/list_ops_test.exs
@@ -56,64 +56,52 @@ defmodule ListOpsTest do
     assert ListOps.map(Enum.to_list(1..1_000_000), &(&1 + 1)) == Enum.to_list(2..1_000_001)
   end
 
-  @tag :not_impl
   test "filter of empty list" do
     assert ListOps.filter([], &odd?/1) == []
   end
 
-  @tag :not_impl
   test "filter of normal list" do
     assert ListOps.filter([1, 2, 3, 4], &odd?/1) == [1, 3]
   end
 
-  @tag :not_impl
   test "filter of huge list" do
     assert ListOps.filter(Enum.to_list(1..1_000_000), &odd?/1) ==
              Enum.map(1..500_000, &(&1 * 2 - 1))
   end
 
-  @tag :not_impl
   test "append of empty lists" do
     assert ListOps.append([], []) == []
   end
 
-  @tag :not_impl
   test "append of empty and non-empty list" do
     assert ListOps.append([], [1, 2, 3, 4]) == [1, 2, 3, 4]
   end
 
-  @tag :not_impl
   test "append of non-empty and empty list" do
     assert ListOps.append([1, 2, 3, 4], []) == [1, 2, 3, 4]
   end
 
-  @tag :not_impl
   test "append of non-empty lists" do
     assert ListOps.append([1, 2, 3], [4, 5]) == [1, 2, 3, 4, 5]
   end
 
-  @tag :not_impl
   test "append of huge lists" do
     assert ListOps.append(Enum.to_list(1..1_000_000), Enum.to_list(1_000_001..2_000_000)) ==
              Enum.to_list(1..2_000_000)
   end
 
-  @tag :not_impl
   test "concat of empty list of lists" do
     assert ListOps.concat([]) == []
   end
 
-  @tag :not_impl
   test "concat of normal list of lists" do
     assert ListOps.concat([[1, 2], [3], [], [4, 5, 6]]) == [1, 2, 3, 4, 5, 6]
   end
 
-  @tag :not_impl
   test "concat of huge list of small lists" do
     assert ListOps.concat(Enum.map(1..1_000_000, &[&1])) == Enum.to_list(1..1_000_000)
   end
 
-  @tag :not_impl
   test "concat of small list of huge lists" do
     assert ListOps.concat(Enum.map(0..9, &Enum.to_list((&1 * 100_000 + 1)..((&1 + 1) * 100_000)))) ==
              Enum.to_list(1..1_000_000)
--- class-day3/learn_about_processes/lib/my_gen_server.ex
+++ class-day3/learn_about_processes/lib/my_gen_server.ex
@@ -1,4 +1,6 @@
 defmodule MyGenServer do
-    def start_link(module, atom_list, keyword_list) do
-    end
+  def start_link(module, atom_list, keyword_list) do
+    if(not is_atom(keyword_list))
+    raise ArgumentError, message: "expected :name to be an atom"
+  end
 end
