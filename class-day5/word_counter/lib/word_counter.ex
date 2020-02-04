a5f94631457be67e80852883776dc6fc0760c1b6
Mike Roncone <mjroncone@gmail.com>
2020-02-04 09:44:37 -0800

    feat: lazy people like hamburgers too

--- class-day5/word_counter/lib/word_counter.ex
+++ class-day5/word_counter/lib/word_counter.ex
@@ -2,16 +2,16 @@ defmodule WordCounter do
   @a_lot_of_words "lib/word_files/a_lot_of_words.txt"
   @r_frost "lib/word_files/rfrost.txt"
 
-  # def benchmark_find() do
-  #   Benchee.run(
-  #     %{
-  #       "eager" => fn -> find_hamburger_eagerly(@a_lot_of_words) end,
-  #       "lazy" => fn -> find_hamburger_lazily(@a_lot_of_words) end
-  #     },
-  #     time: 10,
-  #     memory_time: 2
-  #   )
-  # end
+  def benchmark_find() do
+    Benchee.run(
+      %{
+        "eager" => fn -> find_hamburger_eagerly(@a_lot_of_words) end,
+        "lazy" => fn -> find_hamburger_lazily(@a_lot_of_words) end
+      },
+      time: 10,
+      memory_time: 2
+    )
+  end
 
   # def benchmark_count() do
   #   Benchee.run(
@@ -27,9 +27,16 @@ defmodule WordCounter do
 
   def find_hamburger_eagerly(filepath) do
     filepath
-      |> File.read!()
-      |> String.split("\n")
-      |> Enum.flat_map(&String.split/1)
-      |> Enum.any?(fn(word) -> word == "hamburger" end)
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.flat_map(&String.split/1)
+    |> Enum.any?(fn word -> word == "hamburger" end)
+  end
+
+  def find_hamburger_lazily(filepath) do
+    filepath
+    |> File.stream!([], :line)
+    |> Stream.flat_map(&String.split/1)
+    |> Enum.any?(fn word -> word == "hamburger" end)
   end
 end
